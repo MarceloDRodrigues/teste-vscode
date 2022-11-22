dbde9dd4d2892829ea32c723046d9f258c77e9f5
MarceloDRodrigues <marcelo.lucas02@gmail.com>
2022-11-22 19:53:27 -0300

    sera

---
+++ testinho.java
@@ -0,0 +1,25 @@
+public class testinho {
+
+    public static int multiplicacao(int n, int p){
+        int i;
+        int j;
+        int resultado = 0;
+        int total = 0;
+        for (j=1; j<=p; j++){
+            
+            for (i=1;i<=n;i++){
+                System.out.println(n);
+                resultado = resultado + n; 
+                System.out.println(resultado);   
+                
+            }
+            resultado = n;
+        }
+        System.out.println(total);
+        return total;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Teste");
+
+    }
+}
