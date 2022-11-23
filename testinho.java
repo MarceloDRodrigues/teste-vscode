7765d88f9be2471ead30cca953dc79c9da2cfa4f
MarceloDRodrigues <marcelo.lucas02@gmail.com>
2022-11-23 19:49:52 -0300

    versão final tratando as exceções

--- testinho.java
+++ testinho.java
@@ -1,6 +1,6 @@
-public class App {
+public class testinho {
 
-    public static int multi(int a, int b) {
+    public static int multi(float a, float b) {
         int i;
         int resultado = 0;
         for (i = 0; i < b; i++) {
@@ -10,19 +10,46 @@ public class App {
         return resultado;
     }
 
-    public static int potencia(int x, int y) {
-        int resultado = 1;
-        for (int i = 0; i < y; i++) {
-            resultado = App.multi(resultado, x);
-            System.out.println(resultado);
+    public static float potencia(float x, int y) {
+        float resultado = 1;
+        boolean positivo = true;
+        boolean baseNegativa = false;
+        if (x < 0){
+            if (y % 2 == 0){
+                x = -x;
+            }else{
+                x = -x;
+                 baseNegativa = true;
+            }
         }
+        if (y < 0){
+            positivo = false;
+            y = -y;
+        }
+     
+        for (int i = 0; i < y; i++) {
+            resultado = testinho.multi(resultado, x);
+            
+        }
+        if (positivo != true){
+       
+            resultado = 1 / resultado;
+       
+        }
+        if (baseNegativa == true){
+            resultado = -resultado;
+        }
+        System.out.println(resultado);
         return resultado;
 
     }
 
     public static void main(String[] args) throws Exception {
 
-        App.potencia(6, 3);
+        testinho.potencia(6, -2);
+        testinho.potencia(4, 2);
+        testinho.potencia(0, 2);
+        testinho.potencia(-3, 3);
     }
 }
 
